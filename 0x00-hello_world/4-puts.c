9b9fb6f72f3cddba6ae127a140c0adf4f724f7b8
Leratorikki <richardmarcus000@gmail.com>
2023-08-11 11:36:01 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Return: Always 0 (success)
  */
-int main(void)
+int main(void)6
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
