c9a9d7c3ccfa8970a469960e0d22dc0765c2f238
Leratorikki <richardmarcus000@gmail.com>
2023-08-21 23:39:50 -0700

    pointer

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,10 +7,10 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while (str(i) != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str(i));
-		i++;
+		_putchar(str[i]);
+		
 	}
 	_putchar('\n');
 }
