657ec5d5ffab022787a2d2f665db1182b0cdb619
Leratorikki <richardmarcus000@gmail.com>
2023-08-28 07:17:08 -0700

    Pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,15 +6,14 @@
  * @b: The constant byte value to fill the memory with.
  * @n: The number of bytes to fill.
  *
- * Return: A pointer to the memory areas @s.
+ * Return: A pointer to the memory areas s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int d;
 
 	for (d = 0; d < n; d++)
-	{
 		s[d] = b;
-	}
+
 	return (s);
 }
