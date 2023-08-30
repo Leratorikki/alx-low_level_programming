0724cd04aaff4953c55d89419c66b67148753793
Leratorikki <richardmarcus000@gmail.com>
2023-08-30 07:45:26 -0700

    Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,32 +7,29 @@
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-    {
-        return (0);
-    }
+	if (n <= 1)
+	{
+		return (0);
+	}
 
-    return (is_prime_helper(n, 2));
+	return (is_prime_helper(n, 2));
 }
-
 /**
- * is_prime_helper - Recursive helper function to check if a number is prime.
- * @n: The number to check.
+ * is_prime_helper - recursive helper function to check if a number is a prime.
+ * @n: The number to check
  * @divisor: The current divisor to check.
  *
- * Return: 1 if the number is prime, 0 otherwise.
+ * Return: 1 if the number is a prime, 0 otherwise
  */
 int is_prime_helper(int n, int divisor)
 {
-    if (divisor * divisor > n)
-    {
-        return (1);
-    }
-
-    if (n % divisor == 0)
-    {
-        return (0);
-    }
-
-    return (is_prime_helper(n, divisor + 1));
+	if (divisor * divisor > n)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+	return (is_prime_helper(n, divisor + 1));
 }
