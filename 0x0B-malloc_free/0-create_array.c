d6edb3d571e94286b3300fa482652a5bdd290584
Leratorikki <richardmarcus000@gmail.com>
2023-09-05 07:31:10 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,6 +3,8 @@
 
 /**
  * char *create_array - create array and inizialise it with a apecifial char
+ *@size: the size of memory to allocate
+ *@c: the char to inizialize the array with
  *
  * Return: NULL if size is 0, or NULL to a pointer of array if it fails.
  */
@@ -11,10 +13,16 @@ char *create_array(unsigned int size, char c)
 	char *array = malloc((size + 1) * sizeof(char));
 	unsigned int k;
 
-	if (array == NULL)
+	if (size == 0)
+	{
 		return (NULL);
-	
-	for (k = 0; k < size; k++)
+	}
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (unsigned int k = 0; k < size; k++)
 		{
 			array[k] = c;
 		}
