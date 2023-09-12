267e5b7fc639aaa46bdc975ab3d2ca048a235556
Leratorikki <richardmarcus000@gmail.com>
2023-09-12 13:35:25 -0700

    Type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,31 @@
-#ifndef _0_OBJECT_LIKE_MACRO_H_
-#define _0_OBJECT_LIKE_MACRO_H_
+#ifndef DOG_H
+#define DOG_H
 
-#define SIZE 1024
+/**
+ * struct dog - a dog's basic info
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Who is the owner of the dog
+ *
+ * Description: No description for this struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
