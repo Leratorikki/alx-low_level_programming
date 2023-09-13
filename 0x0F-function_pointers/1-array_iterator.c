e57ccd74416d20d5e52e78138f248dd33911bd37
Leratorikki <richardmarcus000@gmail.com>
2023-09-13 10:45:13 -0700

    Get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,11 +7,14 @@
  * @array: the int array
  * @action: the pointer to the func. to be excuted on each element
  *
+ * Return: Nothing
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t u;
 
+	array == NULL || action = NULL
+		return;
 	for (u = 0; u < size; u++)
 	{
 		action(array[u]);
