acf291606e5227ea17afedb2583234adca1ea26a
Leratorikki <richardmarcus000@gmail.com>
2023-09-13 11:55:05 -0700

    two can do it

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - function that looms for int
+ * @array: an array
+ * @size: the size of array elements
+ * @cmp: the ptr to tge funct tbat is use to compare values
+ *
+ * Return: -1 if size is equal to 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int u;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (u = 0; u < size; u++)
+	{
+		if (cmp(array[u]))
+			return (u);
+	}
+	return (-1);
+}
+
